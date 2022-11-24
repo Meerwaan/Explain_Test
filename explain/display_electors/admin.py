2980e8bd57a72168ef433d4936556cf6bf00e742
Merwan <merwanlaouini@gmail.com>
2022-11-24 21:03:40 +0100

    Creation du superUser: admin + Ajout des modules pour editer les datas et crée de nouvelles data

--- explain/display_electors/admin.py
+++ explain/display_electors/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Territories, TerritoriesParents
+
+admin.site.register(Territories)
+admin.site.register(TerritoriesParents)
--- explain/display_electors/models.py
+++ explain/display_electors/models.py
@@ -2,24 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Territories(models.Model):
-    code=models.CharField(max_length=255)
-    name=models.CharField(max_length=255)
-    kind=models.CharField(max_length=255)
-    created_at=models.DateTimeField()
-    updated_at=models.DateTimeField()
+    code = models.IntegerField()
+    name = models.CharField(max_length=255)
+    kind = models.CharField(max_length=255)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
     is_current = models.BooleanField(default=False)
-    population = models.CharField(max_length=255)
+    population = models.IntegerField()
     official_website_url = models.CharField(max_length=255)
-    articles_count = models.CharField(max_length=255)
-    admin_docs_count = models.CharField(max_length=255)
-    impacters_count = models.CharField(max_length=255)
-    websites_count = models.CharField(max_length=255)
-    sources_count = models.CharField(max_length=255)
-
+    articles_count = models.IntegerField()
+    admin_docs_count = models.IntegerField()
+    impacters_count = models.IntegerField()
+    websites_count = models.IntegerField()
+    sources_count = models.IntegerField()
 
 class TerritoriesParents(models.Model):
-    child_id = models.CharField(max_length=255)
-    parent_id = models.CharField(max_length=255)
+    child_id = models.IntegerField()
+    parent_id = models.IntegerField()
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
